8e5ccbdacd04ce3b952b7a7caa359c72e2f77f6e
HP <13951549405@163.com>
2023-04-17 15:41:48 +0800

    commit

---
+++ src/main/java/com/example/autotest/commonTool/CommonToolClass.java
@@ -0,0 +1,12 @@
+package com.example.autotest.commonTool;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonToolClass {
+
+    public int returnNum(){
+        return 10;
+    }
+
+}
